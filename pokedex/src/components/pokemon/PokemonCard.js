1e028ae01e3e62fc054d0322324e27fcc511d32b
Marcelo Pepis <marcelo.pepis@gmail.com>
2020-09-10 02:14:50 -0300

    Criado e populado o card individual de cada pokemon.

--- pokedex/src/components/pokemon/PokemonCard.js
+++ pokedex/src/components/pokemon/PokemonCard.js
@@ -1,14 +1,63 @@
 import React, { Component } from 'react'
+import styled from "styled-components"
+
+const Sprite = styled.img`
+  width: 5em;
+  height: 5em;
+`;
+
+const Card = styled.div`
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+  &:hover {
+    box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+  }
+  -moz-user-select: none;
+  -website-user-select: none;
+  user-select: none;
+  -o-user-select: none;
+`;
 
 export default class PokemonCard extends Component {
+  state = {
+    name: '',
+    imageURL: '',
+    pokemonIndex: '',
+    height: '',
+    imageLoading: true,
+    toManyRequests: false
+  };
+
+  componentDidMount () {
+    const name = this.props.name;
+    const url = this.props.url;
+    const pokemonIndex = url.split("/")[url.split('/').length - 2];
+    const imageURL = `https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/${pokemonIndex}.png?raw=true`;
+    this.setState({name: name, imageURL: imageURL, pokemonIndex: pokemonIndex});
+  }
+
+
+
   render() {
+    
+
     return (
       <div className="col-md3 col-sm-6 mb-5">
-        <div className="card">
-          <div className="card-header">
-            <h1>Marcelo Teste</h1>
-          </div>
-        </div>
+        <Card className= "card">
+          <h5 className="card-header">{this.state.pokemonIndex}</h5>
+          <Sprite className="card-img-top rounded mx-auto mt-2"
+          onLoad={() => this.setState({imageLoading: false})}
+          onError={() => this.setState({toManyRequests: true})}
+          
+          src= {this.state.imageURL}>
+
+          </Sprite>
+          <div className="card-body mx-auto">
+            <h6 className="card-title">
+              {this.state.name.toLowerCase().split(' ').map(letter => letter.charAt(0).toUpperCase() + letter.substring(1)).join(' ')}
+            </h6>
+          </div>  
+        </Card>
       </div>
 
     )
--- pokedex/src/components/pokemon/PokemonList.js
+++ pokedex/src/components/pokemon/PokemonList.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
 import PokemonCard from './PokemonCard';
+import axios from 'axios';
+import { ThemeConsumer } from 'styled-components';
 
 export default class PokemonList extends Component {
+
+  state = {
+    url: 'https://pokeapi.co/api/v2/pokemon?limit=150',
+    pokemon: null
+  };
+
+   async componentDidMount() {
+    const res = await axios.get(this.state.url);
+    this.setState({ pokemon: res.data['results'] })
+  };
+
   render() {
     return (
-      <div className="row">
-        <PokemonCard></PokemonCard>
-      </div>
+      <React.Fragment>
+        {this.state.pokemon ? (
+          <div className="row"> 
+            {this.state.pokemon.map(pokemon => (
+              <PokemonCard 
+                key = {pokemon.name}
+                name = {pokemon.name}
+                url = {pokemon.url} 
+              />
+            ))}
+          </div>
+        ) : (
+          <h1>Crregando lista de Pokemons</h1>
+        )}
+      </React.Fragment>
     )
   }
 }
